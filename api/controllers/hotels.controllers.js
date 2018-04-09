69fed25efb1937d63211690c7f1cd5529817f439
SunYuan <reggie.sunyuan@me.com>
2018-04-09 07:09:38 -0400

    add doc to mongodb

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -133,28 +133,43 @@ module.exports.hotelsGetOne = function(req, res) {
     
 }
 
-module.exports.hotelsAddOne = function(req, res) {
-    var db = dbconn.get();
-    var collection = db.db().collection(collectionName);
-    var newHotel;
-    console.log("POST new hotel");
-
-    if(req.body && req.body.name && req.body.stars) {
-        newHotel = req.body;
-        newHotel.stars = parseInt(req.body.stars, 10);
-        
-        collection.insertOne(newHotel, function(err, response) {
-            console.log(response.ops);
-            res
-            .status(201) // status code for new resource being created is 201
-            .json(response.ops);
-        });
-        
+var _splitArray = function(input) {
+    var output;
+    if(input && input.length > 0) {
+        output = input.split(";");
     } else {
-        res
-        .status(400)
-        .json({message: 'Required data missing from body'});
+        output = [];
     }
+    return output;
+}
 
-    
+module.exports.hotelsAddOne = function(req, res) {
+    Hotel
+        .create({
+            name: req.body.name,
+            description: req.body.description,
+            stars: parseInt(req.body.stars, 10),
+            services: _splitArray(req.body.services),
+            photos: _splitArray(req.body.photos),
+            currency: req.body.currency,
+            location: {
+                address: req.body.address,
+                coordinates: [
+                    parseFloat(req.body.lng), 
+                    parseFloat(req.body.lat)
+                ]
+            }
+        }, function(err, hotel){
+            if(err) {
+                console.log("Error creating hotel");
+                res
+                    .status(400)
+                    .json(err);
+            } else {
+                console.log("Hotel created ", hotel);
+                res
+                    .status(201)
+                    .json(hotel);
+            }
+        });
 }
--- api/controllers/reviews.controller.js
+++ api/controllers/reviews.controller.js
@@ -40,3 +40,56 @@ module.exports.reviewsGetOne = function (req, res) {
         }
     });
 };
+
+var _addReview = function(req, res, hotel) {
+    hotel.reviews.push({
+        name : req.body.name,
+        rating: parseInt(req.body.rating, 10),
+        review: req.body.review
+    });
+    hotel.save(function(err, hotelUpdated) {
+        if(err) {
+            res
+                .status(500)
+                .json(err);
+        } else {
+            res
+                .status(201)
+                .json(hotelUpdated.reviews[hotelUpdated.reviews.length - 1]);
+        }
+    });
+};
+
+module.exports.reviewsAddOne = function(req, res) {
+    var hotelId = req.params.hotelId;
+    console.log('GET hotelId ', hotelId);
+
+    Hotel.findById(hotelId)
+    .select('reviews') // only pick review doc, instead of all info of the hotel
+    .exec(function(err, hotel) {
+        var response =  {
+            status: 200,
+            message: []
+        };
+        if(err) {
+            console.log("Error finding hotel");
+            response.status = 500;
+            response.message = err;
+        } else if (!hotel) {
+            console.log("Hotel id not found in database", id);
+            response.status = 404;
+            response.message = {
+                "message" : "Hotel ID not found " + id
+            };
+        } else {
+            response.message = hotel.reviews? hotel.reviews : [];
+        }
+        if(hotel) {
+            _addReview(req, res, hotel);
+        } else {
+            res
+            .status(response.status)
+            .json(response.message);
+        }
+    });
+}
